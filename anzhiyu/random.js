549f874b3d948c24c3363a3afed89c949b0b60d4
Fangtang74 <izhaorm@qq.com>
2024-01-05 17:00:30 +0800

    Site updated: 2024-01-05 17:00:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1d57a7e730dc.html","posts/0331af577420.html","posts/1e36cdae905a.html"];function toRandomPost(){
+var posts=["posts/1e36cdae905a.html","posts/0331af577420.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
