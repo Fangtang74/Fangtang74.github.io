62454ad2f8192a63dca75267d20522407a032b2f
Fangtang74 <izhaorm@qq.com>
2024-03-21 17:07:30 +0800

    Site updated: 2024-03-21 17:07:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/08a1e1c0167f.html","posts/1e36cdae905a.html","posts/1d57a7e730dc.html","posts/0331af577420.html"];function toRandomPost(){
+var posts=["posts/1e36cdae905a.html","posts/08a1e1c0167f.html","posts/0331af577420.html","posts/1d57a7e730dc.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
