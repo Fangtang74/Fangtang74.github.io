59f422b572cbc90588a7cfd0b6eecab4e8996512
Fangtang74 <izhaorm@qq.com>
2024-05-21 17:29:47 +0800

    Site updated: 2024-05-21 17:29:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1e36cdae905a.html","posts/0331af577420.html","posts/1d57a7e730dc.html","posts/6226571ae252.html","posts/80fc9384f990.html"];function toRandomPost(){
+var posts=["posts/80fc9384f990.html","posts/6226571ae252.html","posts/1e36cdae905a.html","posts/1d57a7e730dc.html","posts/0331af577420.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
